b6448ab4579cc3964bf225748d1f4a3b3a40359a
Rudegog <matheusgf1500@hotmail.com>
2022-11-08 17:29:40 -0300

    Adicionando calculadora

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,31 @@
-document.getElementById("myInput").oninvalid = function() {myFunction()};
+// document.getElementById("myInput").oninvalid = function() {myFunction()};
 
-function myFunction() {
-  alert("Você precisa completar as perguntinhas.");
-}
+// function myFunction() {
+//   alert("Você precisa completar as perguntinhas.");
+// }
 
-document.querySelector("myInput").addEventListener("input", function(){
-   
- const go = document.querySelector("email");
-  
-  go.disabled = this.value.length >= 3 ? false : true;
-  
+document.getElementById("submit").disabled = true;
+document.getElementById("input").addEventListener("input", function(event){
+
+  //busca conteúdo do input
+    const conteudo = document.getElementById("input").value;
+
+    //valida conteudo do input 
+    if (conteudo !== null && conteudo !== '') {
+      //habilita o botão
+      document.getElementById("submit").disabled = false;
+    } else {
+      //desabilita o botão se o conteúdo do input ficar em branco
+      document.getElementById("submit").disabled = true;
+    }
 });
 
-function IsEmail(email){
-  var exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
-  var check=/@[w-]+./;
-  var checkend=/.[a-zA-Z]{2,3}$/;
-  if(((email.search(exclude) != -1)||(email.search(check)) == -1)||(email.search(checkend) == -1)){return false;}
-  else {return true;}
-}
+
+
+// function myInput(myInput){
+//   const exclude=/[^@-.w]|^[_@.-]|[._-]{2}|[@.]{2}|(@)[^@]*1/;
+//   const check=/@[w-]+./;
+//   const checkend=/.[a-zA-Z]{2,3}$/;
+//   if(((myInput.search(exclude) != -1)||(myInput.search(check)) == -1)||(myInput.search(checkend) == -1)){return false;}
+//   else {return true;}
+// }
