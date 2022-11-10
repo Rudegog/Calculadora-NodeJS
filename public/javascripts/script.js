2ad5a3ed29adcb320ee9ea2f35e5aa75906181e5
Rudegog <matheusgf1500@hotmail.com>
2022-11-10 17:34:37 -0300

    Alterando rotas

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,13 +3,14 @@ function myFunction() {
   alert("Você precisa completar as perguntinhas.");
 }
 
-document.getElementById("submit").disabled = true;
-document.getElementById("myInput").addEventListener("myInput", function (event) {
-  const conteudo = document.getElementById("myInput").value;
+//VOU MELHORAR ESTE CÓDIGO
 
-  if (conteudo !== null && conteudo !== '') {
-    document.getElementById("submit").disabled = false;
-  } else {
-    document.getElementById("submit").disabled = true;
-  }
-});
+// document.getElementById("submit").disabled = true;
+// document.getElementById("myInput").addEventListener("myInput", function (event) {
+//   const conteudo = document.getElementById("myInput").value;
+//   if (conteudo !== null && conteudo !== '') {
+//     document.getElementById("submit").disabled = false;
+//   } else {
+//     document.getElementById("submit").disabled = true;
+//   }
+// });
